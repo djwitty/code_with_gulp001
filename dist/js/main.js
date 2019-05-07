e045b77811d63e0fc01f239e69ce2e1f887ce185
Kazinaki <mikhail@appscloud.io>
2019-05-07 18:23:24 +0300

    new file:   dist/css/libs.min.css 	new file:   dist/css/main.min.css 	new file:   dist/fonts/verdana.ttf 	new file:   dist/index.html 	new file:   dist/js/libs.js 	new file:   dist/js/main.js 	new file:   dist/js/main.js.map 	new file:   gulp/build.js 	new file:   gulp/default.js 	new file:   gulp/fonts.js 	new file:   gulp/html.js 	new file:   gulp/img.js 	new file:   gulp/js.js 	new file:   gulp/libs.js 	new file:   gulp/sass.js 	new file:   gulp/server.js 	new file:   gulpfile.js 	new file:   package-lock.json 	new file:   package.json 	new file:   src/css/libs.css 	new file:   src/css/libs/1.css 	new file:   src/fonts/verdana.ttf 	new file:   src/img/1.png 	new file:   src/index.html 	new file:   src/js/components/functions.js 	new file:   src/js/libs.js 	new file:   src/js/libs/2.js 	new file:   src/js/main.js 	new file:   src/sass/components/variables.sass 	new file:   src/sass/main.sass 	new file:   src/sass/page/_index.sass 	new file:   src/templates/footer.html 	new file:   src/templates/head.html 	new file:   src/templates/header.html

---
+++ dist/js/main.js
@@ -0,0 +1,2 @@
+"use strict";function helloWorld(){console.log("Hello World !123")}function summ(o,l){var e=o+l;console.log(e)}console.log("999"),helloWorld(),summ(16,19);
+//# sourceMappingURL=main.js.map

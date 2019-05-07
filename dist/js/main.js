8db5b9f380f8299cec7dfa8fc17b6b74f5830da7
Kazinaki <mikhail@appscloud.io>
2019-05-07 18:24:40 +0300

    deleted:    dist/css/libs.min.css 	deleted:    dist/css/main.min.css 	deleted:    dist/fonts/verdana.ttf 	deleted:    dist/index.html 	deleted:    dist/js/libs.js 	deleted:    dist/js/main.js 	deleted:    dist/js/main.js.map

--- dist/js/main.js
+++
@@ -1,2 +0,0 @@
-"use strict";function helloWorld(){console.log("Hello World !123")}function summ(o,l){var e=o+l;console.log(e)}console.log("999"),helloWorld(),summ(16,19);
-//# sourceMappingURL=main.js.map
